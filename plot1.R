fe81382550ccae3105fdc6f8676e23113e73d2f4
ppopli <ppopli2793@gmail.com>
2015-02-06 11:11:37 +0530

    updated plot1

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stri
 data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ##Opening Graphic device(png)
-png(file = "plot4.png", height = 480, width =480)
+png(file = "plot1.png", height = 480, width =480)
 
 hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
 ##Closing Graphic Device
